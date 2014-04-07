ebe4abc49020b10e325cd82f5f6cfa401b916b48
Vagios Vlachos <bagios234@gmail.com>
2014-04-08 02:19:32 +0300

    Add form to add tasks

--- main.rb
+++ main.rb
@@ -4,3 +4,13 @@ require 'slim'
 get '/' do
 	slim :index
 end
+
+get '/:task' do
+	@task = params[:task].split('-').join(' ').capitalize
+	slim :task
+end
+
+post '/' do
+	@task = params[:task]
+	slim :task
+end
