642fbfc73f3846ecff00517e3eb0a07f6c2e360b
Vagios Vlachos <bagios234@gmail.com>
2014-04-08 02:12:35 +0300

    Create views folder and organize views

--- main.rb
+++ main.rb
@@ -4,24 +4,3 @@ require 'slim'
 get '/' do
 	slim :index
 end
-
-__END__
-
-@@layout
-doctype html
-html
-	head
-		meta charset="utf-8"
-		title To Do App
-		link rel="stylesheet" media="screen, projection" href="assets/styles/style.css"
-		/[if lt IE 9] 
-      		script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
-    body
-    	h1 To Do App
-    	== yield
-
-
-@@index
-h2 My Tasks
-ul.tasks
-	li This is a task		
