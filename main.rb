11012ef0c9422bd41a6d043226cd6f80c106f6ff
Vagios Vlachos <bagios234@gmail.com>
2014-04-08 03:03:13 +0300

    Add database with tasks. Tasks can now be added, completed, deleted

--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'data_mapper'
 require 'slim'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :completed_at, DateTime
+end
+
+DataMapper.finalize
+
 get '/' do
+	@tasks = Task.all
 	slim :index
 end
 
@@ -11,6 +24,18 @@ get '/:task' do
 end
 
 post '/' do
-	@task = params[:task]
-	slim :task
+	Task.create params[:task]
+	redirect to('/')
+end
+
+delete '/task/:id' do
+	Task.get(params[:id]).destroy
+	redirect to('/')
+end
+
+put '/task/:id' do
+	task = Task.get params[:id]
+	task.completed_at = task.completed_at.nil? ? Time.now : nil
+	task.save
+	redirect to('/')
 end
