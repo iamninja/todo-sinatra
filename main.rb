8153c389f36eb320f4206a876e496a5c911a766a
Vagios Vlachos <bagios234@gmail.com>
2014-04-08 17:57:55 +0300

    Add lists of tasks

--- main.rb
+++ main.rb
@@ -6,15 +6,22 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/developm
 
 class Task
   include DataMapper::Resource
-  property :id,           Serial
-  property :name,         String, :required => true
-  property :completed_at, DateTime
+  property :id,           	Serial
+  property :name,         	String, :required => true
+  property :completed_at, 	DateTime
+end
+
+class List
+	include DataMapper::Resource
+	property :id,			Serial
+	property :name,			String, :required => true
+	has n, :tasks, :constraint => :destroy
 end
 
 DataMapper.finalize
 
 get '/' do
-	@tasks = Task.all
+	@lists = List.all(:order => [:name])
 	slim :index
 end
 
@@ -23,8 +30,8 @@ get '/:task' do
 	slim :task
 end
 
-post '/' do
-	Task.create params[:task]
+post '/:id' do
+	List.get(params[:id]).tasks.create params['task']
 	redirect to('/')
 end
 
@@ -37,5 +44,15 @@ put '/task/:id' do
 	task = Task.get params[:id]
 	task.completed_at = task.completed_at.nil? ? Time.now : nil
 	task.save
+	redirect to('/')
+end
+
+post '/new/list' do
+	List.create params['list']
+	redirect to('/')
+end
+
+delete '/list/:id' do
+	List.get(params[:id]).destroy
 	redirect to('/')
 end
